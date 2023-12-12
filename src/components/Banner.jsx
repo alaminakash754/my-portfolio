b48efb500155573032ecf3f55f338d7e4767713a
alamin <alaminakash9754@gmail.com>
2023-12-12 19:48:36 +0600

    downloaded cv

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import coverPhoto from '../assets/my pic.jpg';
 import { MdDownload } from "react-icons/md";
+import resume from '../assets/personal branding.pdf'
 
 const Banner = () => {
     return (
@@ -8,7 +9,7 @@ const Banner = () => {
                 <h1 className="text-5xl">Hi, I'm Alamin!</h1>
 
                 <p className='mt-10 text-justify font-semibold text-lg'>Welcome to my portfolio! I specialize in crafting immersive user experiences as a front-end developer. With a passion for clean code and innovative design, I bring concepts to life using HTML, CSS, and JavaScript. Proficient in frameworks like React and Vite, I strive to create responsive and visually stunning interfaces that elevate user engagement and satisfaction.</p>
-                <button className='bg-green-600 rounded-lg mt-10 p-3 text-xl font-medium flex items-center justify-center gap-3 text-center'>Download Resume <p className='mt-2'><MdDownload/></p></button>
+                <a href={resume} download="Resume"><button className='bg-green-600 rounded-lg mt-10 p-3 text-xl font-medium flex items-center justify-center gap-3 text-center'>Download Resume <p className='mt-2'><MdDownload/></p></button></a>
             </div>
             <div className="ml-14">
                 <img className='rounded-full w-[400px] h-[400px] ' src={coverPhoto} alt="" />
