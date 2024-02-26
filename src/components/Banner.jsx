8ef0e6589b311d32e316e208caa321c2149bbdb0
alamin <alaminakash9754@gmail.com>
2024-02-27 00:16:54 +0600

    changes in banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,15 +9,7 @@ import '../style/style.css'
 const Banner = () => {
     return (
         <div className=" grid sm:grid-cols-1 md:grid-cols-2  gap-10 h-[500px] p-6">
-            <div className=" ml-5">
-                <h1 className="text-5xl">Hi, I'm Alamin!</h1>
-
-                <p className='mt-10 text-justify font-semibold text-lg'>Welcome to my portfolio! I specialize in crafting immersive user experiences as a front-end developer. With a passion for clean code and innovative design, I bring concepts to life using HTML, CSS, and JavaScript. Proficient in frameworks like React and Vite, I strive to create responsive and visually stunning interfaces that elevate user engagement and satisfaction.</p>
-                <a href={resume} download="Resume"><button className='bg-green-600 rounded-lg mt-10 p-3 text-xl font-medium flex items-center justify-center gap-3 text-center transform hover:scale-110 hover:bg-opacity-50 end-auto duration-300   shadow-lime-400 hover:transition-transform hover:bg-origin-border'>Download Resume <p className='mt-2'><MdDownload/></p></button></a>
-
-                <Link to='about'><button className="button"> <span className="button__icon"><FaArrowRight className="mt-5 ml-5" /></span> <span>More About me</span>  </button></Link>
-            </div>
-            <div className="ml-14">
+             <div className="ml-14">
                 <div className='box'>
                     <div className='content'>
                         <img src={coverPhoto} alt="" />
@@ -26,6 +18,15 @@ const Banner = () => {
                     </div>
                 </div>
             </div>
+            <div className=" ml-5">
+                <h1 className="text-5xl">Hi, I'm Alamin!</h1>
+
+                <p className='mt-10 text-justify font-semibold text-lg'>Welcome to my portfolio! I specialize in crafting immersive user experiences as a front-end developer. With a passion for clean code and innovative design, I bring concepts to life using HTML, CSS, and JavaScript. Proficient in frameworks like React and Vite, I strive to create responsive and visually stunning interfaces that elevate user engagement and satisfaction.</p>
+                <a href={resume} download="Resume"><button className='bg-green-600 rounded-lg mt-10 p-3 text-xl font-medium flex items-center justify-center gap-3 text-center transform hover:scale-110 hover:bg-opacity-50 end-auto duration-300   shadow-lime-400 hover:transition-transform hover:bg-origin-border'>Download Resume <p className='mt-2'><MdDownload/></p></button></a>
+
+                <Link to='about'><button className="button"> <span className="button__icon"><FaArrowRight className="mt-5 ml-5" /></span> <span>More About me</span>  </button></Link>
+            </div>
+           
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,67 @@
-import { NavLink } from 'react-router-dom';
-import logo from '../assets/logo.jpg'
+// import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../style/style.css';
+// import logo from '../assets/logo.jpg'
+
 const Navbar = () => {
+    
 
-    const navLinks = <>
-        <li><NavLink to="/"
-            className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? " font-bold text-lg text-green-400 hover:bg-gray-200 hover:text-green-400 " : ""
-            }>Home</NavLink></li>
+    // const navLinks = <>
+    //     <li><NavLink to="/"
+    //         className={({ isActive, isPending }) =>
+    //             isPending ? "pending" : isActive ? " font-bold text-lg text-green-400 hover:bg-gray-200 hover:text-green-400 " : " hover:text-green-400"
+    //         }>Home</NavLink></li>
             
-        <li><NavLink to="/projects"
-            className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? " font-bold  text-lg text-green-400 hover:bg-gray-200 hover:text-green-400  " : ""
-            }>My Projects</NavLink></li>
+    //     <li><NavLink to="/projects"
+    //         className={({ isActive, isPending }) =>
+    //             isPending ? "pending" : isActive ? " font-bold  text-lg text-green-400 hover:bg-gray-200 hover:text-green-400  " : " hover:text-green-400"
+    //         }>My Projects</NavLink></li>
 
 
 
-        <li><NavLink to="/contact"
-            className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? " text-lg text-green-400 hover:bg-gray-200 hover:text-green-400 font-bold " : ""
-            }>Contact</NavLink></li>
+    //     <li><NavLink to="/contact"
+    //         className={({ isActive, isPending }) =>
+    //             isPending ? "pending" : isActive ? " text-lg text-green-400 hover:bg-gray-200 hover:text-green-400 font-bold " : " hover:text-green-400"
+    //         }>Contact</NavLink></li>
 
-    </>
+    // </>
     return (
-        <div className="navbar bg-base-200 max-w-[1160px] mx-auto">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        {navLinks}
-                    </ul>
-                </div>
-                <img className='w-48 h-24 rounded-3xl' src={logo} alt="" />
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navLinks}
-                </ul>
-            </div>
-            <div className="navbar-end mr-8">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                    </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-32">
-                        {navLinks}
-                    </ul>
-                </div>
-            </div>
-        </div>
+        // <div className="navbar bg-base-200 max-w-[1160px] mx-auto">
+        //     <div className="navbar-start">
+        //         <div className="dropdown">
+        //             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        //                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        //             </div>
+        //             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        //                 {navLinks}
+        //             </ul>
+        //         </div>
+        //         <img className='w-48 h-24 rounded-3xl' src={logo} alt="" />
+        //     </div>
+        //     <div className="navbar-center hidden lg:flex">
+        //         <ul className="menu menu-horizontal px-1">
+        //             {navLinks}
+        //         </ul>
+        //     </div>
+        //     <div className="navbar-end mr-8">
+        //         <div className="dropdown">
+        //             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        //                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+        //             </div>
+        //             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-32">
+        //                 {navLinks}
+        //             </ul>
+        //         </div>
+        //     </div>
+        // </div>
+        <nav className='nav_menu'>
+            <ul>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='projects'><li>My Projects</li></Link>
+                <Link to='contact'><li>Contact</li></Link>
+                
+            </ul>
+        </nav>
     );
 };
 
